84d4aa3f050d17c6cce12b031eccdb5a219a77a7
Anna Lopatina <an.lopatina050@gmail.com>
2021-02-26 17:44:20 +0200

    Add custom divide exception

---
+++ Exception_Practice/DivideException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exception_Practice
+{
+    class DivideException : Exception
+    {
+        public DivideException() : base() {}
+
+        public DivideException(string message) : base(message) {}
+    }
+}
--- Exception_Practice/Program.cs
+++ Exception_Practice/Program.cs
@@ -6,7 +6,31 @@ namespace Exception_Practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine(Divide(10, 5));
+            }
+            catch (DivideException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine(Divide(10, 3));
+            }
+            catch (DivideException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static int Divide(int a, int b)
+        {
+            if (a % b == 0)
+                return a / b;
+            else
+                throw new DivideException("The result of divide is not an integer.");
         }
     }
 }
